a203dc003506ba587b765e9b6f6e3d3b94ddb37e
luojia <luojia@luojia.me>
2018-06-20 13:45:35 +0800

    no message

--- README.md
+++ README.md
@@ -17,23 +17,34 @@ Usage: namer [options]
     -f, --find <rexexp>          Regexp for finding target string (default: /.*/)
     -r, --replace <replacement>  Replacement string (default: )
     -m, --match <rexexp>         Regexp for matching files (default: the same as find)
+    --keep-order                 Do not reorder the file list. By default the namer will reorder file names by their length.
     -y, --yes                    Do not ask for confirmation
     -h, --help                   output usage information
 ```
 
 Regexps in the parameters can be with or without // warper(The wraper can be used when you need regexp flags "i" or "g").
 
+You may need to add "\" before some signs in your command environment.
 
 The replacement will become the second parameter of string.replace function in javascript. So several special signs can be used.
-See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_string_as_a_parameter
-    
-There is an extra special replacement pattern "#COUNTER" which is a counter that shows the index of the matched file(starts from 1).
+
+### Patterns
+```
+Special javascript replacement patterns
+    $$            Inserts a "$"
+    $&            Inserts the matched substring
+    $`            Inserts the portion of the string that precedes the matched substring
+    $'            Inserts the portion of the string that follows the matched substring
+    $n            Insert the nth submatch group, from 1 ot 100
+Extra replacement patterns
+    #COUNTER      Inserts a counter number which is the index of the file in the match list (starts from 1)
+```
 
 ### Examples:
 
 ```shell
-    namer -m w.s -f poi                 #remove string matches /poi/ from names that matches /w.s/
-    namer -f "p(o+)i" -r "$1"           #(special replacement patterns)cut "p" and "i" sticks to the "o"s for files that can be matched
-    namer -f /aaaaaa/i -r b             #(ignore case)replace /aaaaaa/i mode to "b" for files that can be matched
-    namer -f some*pics\.png -r "#COUNTER.png" #change the names to numbers for some png files that can be matched
+    namer -m w.s -f poi                        #remove string matches /poi/ from names that matches /w.s/
+    namer -f "p(o+)i" -r "$1"                  #(special replacement patterns)cut "p" and "i" sticks to the "o"s for files that can be matched
+    namer -f /aaaaaa/i -r b                    #(ignore case)replace /aaaaaa/i mode to "b" for files that can be matched
+    namer -f some*pics\.png -r "#COUNTER.png"  #change the names to numbers for some png files that can be matched
 ```
--- namer.js
+++ namer.js
@@ -9,19 +9,32 @@ commander
 .option('-f, --find <rexexp>', 'Regexp for finding target string',parseRegExp,/.*/)
 .option('-r, --replace <replacement>', 'Replacement string',String,'')
 .option('-m, --match <rexexp>', 'Regexp for matching files (default: the same as find)',parseRegExp)
+.option('--keep-order', 'Do not reorder the file list. By default the namer will reorder file names by their length.')
 .option('-y, --yes', 'Do not ask for confirmation');
 commander.on('--help', function(){
-	console.log();
-	console.log('    Regexps in the parameters can be with or without // warper(The wraper can be used when you need regexp flags "i" or "g").');
-	console.log('    The replacement will become the second parameter of string.replace function in javascript. So several special signs can be used.\nSee https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_string_as_a_parameter');
-	console.log('    There is an extra special replacement pattern "#COUNTER" which is a counter that shows the index of the matched file(starts from 1).');
-	console.log('  Examples:');
-	console.log();
-	console.log('    renamer -m w.s -f poi                 #remove string matches /poi/ from names that matches /w.s/');
-	console.log('    renamer -f "p(o+)i" -r "$1"             #(special replacement patterns)cut "p" and "i" sticks to the "o"s for files that can be matched');
-	console.log('    renamer -f /aaaaaa/i -r b             #(ignore case)replace /aaaaaa/i mode to "b" for files that can be matched');
-	console.log('    renamer -f some*pics\\.png -r "#COUNTER.png" #change the names to numbers for some png files that can be matched');
-	console.log();
+	[
+		'',
+		'    Regexps in the parameters can be with or without // warper(The wraper can be used when you need regexp flags "i" or "g").',
+		'    You may need to add "\\" before some signs in your command environment.',
+		'    The replacement will become the second parameter of string.replace function in javascript. So several special signs can be used.',
+		'',
+		'  Special javascript replacement patterns',
+		'    $$            Inserts a "$"',
+		'    $&            Inserts the matched substring',
+		'    $`            Inserts the portion of the string that precedes the matched substring',
+		'    $\'            Inserts the portion of the string that follows the matched substring',
+		'    $n            Insert the nth submatch group, from 1 ot 100',
+		'  Extra replacement patterns',
+		'    #COUNTER      Inserts a counter number which is the index of the file in the match list (starts from 1)',
+		'',
+		'  Examples:',
+		'',
+		'    namer -m w.s -f poi                        #remove string matches /poi/ from names that matches /w.s/',
+		'    namer -f "p(o+)i" -r "$1"                  #(special replacement patterns)cut "p" and "i" sticks to the "o"s for files that can be matched',
+		'    namer -f /aaaaaa/i -r b                    #(ignore case)replace /aaaaaa/i mode to "b" for files that can be matched',
+		'    namer -f some*pics\\.png -r "#COUNTER.png"  #change the names to numbers for some png files that can be matched',
+		'',
+	].forEach(function(l){console.log(l)});
 });
 
 
@@ -36,13 +49,18 @@ var cwd=process.cwd();
 var counter=0;
 //match files
 var dirList=fs.readdirSync(cwd);
+//order the list
+if(commander.keepOrder!==true)
+	dirList.sort(function(pre,aft){
+		return pre.length-aft.length;
+	});
 console.log("Match list:");
 dirList.forEach(function(name){
 	if(name==='.'||name==='..')return;
 	if(name.match(commander.match)){
 		counter++;
 		var newName=name.replace(commander.find,commander.replace).replace(/\#COUNTER/g,counter);
-		console.log(name,"		>		"+newName);
+		console.log(name,"\t>\t"+newName);
 		replaceList.push([name,newName]);
 	}
 });
