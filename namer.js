c36a4cc60b1759066b4f123d370cd012314a42bb
luojia <luojia@luojia.me>
2022-05-14 19:01:31 +0800

    add exclude filter

--- namer.js
+++ namer.js
@@ -6,23 +6,25 @@ const readline = require('readline');
 const Path = require('path');
 
 commander
-.option('-R, --recursive', 'Match child directories recursively')
-.option('-f, --filter <regexp>', 'Filter for finding target files (default: same as --match)',parseRegExp)
-.option('-r, --replace <replacement>', 'Replacement string',String,'')
-.option('-m, --match <regexp>', 'Regexp for matching strings to be replaced',parseRegExp,"/.*/")
+.option('-R, --recursive', 'Match child directories recursively.')
+.option('-f, --filter <regexp>', 'Filter for finding target files (default: value of --match).',parseRegExp)
+.option('-r, --replace <replacement>', 'Replacement string.',String,'')
+.option('-m, --match <regexp>', 'Regexp for matching strings to be replaced.',parseRegExp,"/.*/")
+.option('-l, --list', 'Just list filtered files and directories.')
 .option('--keep-order', 'Do not reorder the file list. By default the namer will reorder file names by their length.')
-.option('-y, --yes', 'Do not ask for confirmation')
+.option('-y, --yes', 'Do not ask for confirmation.')
 .option('--no-folder', 'Do not change folder\'s name.',false)
-.option('--no-file', 'Do not change file\'s name.',false);
+.option('--no-file', 'Do not change file\'s name.',false)
+.option('-e, --exclude [regexps...]', 'Define exclude filter regexp for relative path.');
 commander.on('--help', function(){
 console.log(
 `
 
- This tool replaces matches to replacements on filtered files
-    Regexps in the parameters can be with or without // surrounded(This can be used when you need regexp flags "i" or "g").
-    You may need to add "\\" before some signs in your command environment.
-    You may need to surround the regexp with \'\' if special sign appears.
-    The replacement will become the second parameter of \'string.replace\' function in javascript. So several special signs can be used.
+ This tool replaces matches(-m) to replacements(-r) on filtered(-f) files
+    Regexps in the parameters can be with or without "//" surrounded(This can be used when you need regexp flags "i" or "g").
+    You may need to add "\\" before some characters in your command environment.
+    You may need to surround the regexp with \'\'(single quotes) if special characters appears.
+    The replacement will become the second parameter of \'string.replace\' function in javascript. So several special patterns can be used in it.
 
   Special javascript replacement patterns
     $$            Inserts a "$"
@@ -32,6 +34,11 @@ console.log(
     $n            Insert the nth submatch group, from 1 ot 100
   Extra replacement patterns
     #COUNTER      Inserts a counter number which is the index of the file in the filter list (starts from 1)
+
+  Path exclude
+    Use "--exclude" option for defining path exclude filter regexp, can be multi-values devided by space.
+	"-f" and "-m" are for file names only, this option filters the relative path of the target. 
+	Path seperator is "/" and starts with no "./" .
 
   Examples:
 
@@ -51,23 +58,27 @@ console.log(
 )
 });
 
+commander.parse();
+const options = commander.opts();
 
-commander.parse(process.argv);
 //set match to find if not set
-if(commander.filter===undefined)commander.filter=commander.match;
-console.log('filter:',commander.filter,"  match:",commander.match,"  replacement:",commander.replace);
+if(options.filter===undefined)options.filter=options.match;
+console.log('Working dir:',`"${process.cwd()}"`);
+console.log('filter:',options.filter,"  match:",options.match,"  replacement:",options.replace);
+
+if(options.exclude){
+	options.exclude=options.exclude.map(s=>parseRegExp(s));
+	console.log('exclude filters: ',options.exclude);
+}
 
 var replaceList=[];
 var counter=0;
 var workRoot='.';
 //find files
-console.log("Change list:");
 findIn(workRoot);
-console.log('\n');
-
-console.log(counter+" files found, "+replaceList.length+" names will be changed.");
-if(replaceList.length===0)return;
-if(commander.yes!==true){
+console.log(`\n${counter} files found${!options.list?", "+replaceList.length+" names will be changed.":'.'}`);
+if(replaceList.length===0 || options.list)process.exit(0);//no file to be renamed or in list mode
+if(options.yes!==true){
 	const rl = readline.createInterface({
 	  input: process.stdin,
 	  output: process.stdout
@@ -82,41 +93,55 @@ if(commander.yes!==true){
 function findIn(dir){
 	//match files
 	try{
-		var dirList=fs.readdirSync(dir);
+		var dirItems=fs.readdirSync(dir);
+		console.log(dirItems);
 	}catch(e){
 		console.error('Failed to scan '+dir,`[${e.message}]`);
 		return;
 	}
 	//order the list
-	if(commander.keepOrder!==true){
-		sort(dirList);
+	if(options.keepOrder!==true){
+		sort(dirItems);
 	}
 	let r;
 	let tabs=(r=dir.match(new RegExp(`\\${Path.sep}`,'g')))?r.length:0;
 	let dirLogged=false;
-	dirList.forEach(function(name){
+	dirItems.forEach(function(name){
 		if(name==='.'||name==='..')return;
 		let fpath=`${dir}/${name}`;
+		if(options.exclude){
+			let relPath=fpath.replace(/^\.\//,'');
+			console.log(relPath);
+			for(let regexp of options.exclude){
+				console.log(regexp);
+				if(regexp.test(relPath))return;
+			}
+		}
 		try{
 			var stat=fs.statSync(fpath);
 		}catch(e){
 			console.error('Failed to get stat of '+fpath,`[${e.message}]`);
 			return;
 		}
-		if((!commander.folder&&stat.isDirectory()) || (!commander.file&&stat.isFile())){}
-		else if(name.match(commander.filter)){
+		if((!options.folder&&stat.isDirectory()) || (!options.file&&stat.isFile())){}
+		else if(name.match(options.filter)){
 			counter++;
-			var newName=name.replace(commander.match,commander.replace).replace(/\#COUNTER/g,counter);
+			if(counter===1){
+				console.log("List:");
+			}
+			var newName=name.replace(options.match,options.replace).replace(/\#COUNTER/g,counter);
 			if(name!==newName){
 				if(!dirLogged){
 					console.log('|   '.repeat(tabs)+'[Dir: '+dir+']');
 					dirLogged=true;
 				}
-				console.log(`${'|   '.repeat(tabs+1)}<${name}>`,`\n${'|   '.repeat(tabs+1)} → `+newName);
-				replaceList.push([Path.join(dir,name),Path.join(dir,newName)]);
+				if(!options.list){
+					console.log(`${'|   '.repeat(tabs+1)}<${name}>`,`\n${'|   '.repeat(tabs+1)} → `+newName);
+					replaceList.push([Path.join(dir,name),Path.join(dir,newName)]);
+				}
 			}
 		}
-		if(stat.isDirectory()&&commander.recursive){
+		if(stat.isDirectory()&&options.recursive){
 			findIn(fpath);
 		}
 	});
